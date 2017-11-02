9158323f887ccfb22e44a271066fd93333aceed0
manish-aloha1 <manish-aloha1@gmail.com>
2017-11-03 04:30:33 +0530

    MA add new api

--- models/orderModel.js
+++ models/orderModel.js
@@ -4,7 +4,17 @@ var	appModel = require('./appModel'),
 var order = {
 	getAction : function(data, res) {
 		appModel.ctPool.query(order.createQuery(data), function(err, result) {
-			res(err, _.groupBy(result, function(res){ return res.customer_id; }));
+			var orders = []
+			_.map(_.groupBy(result, function(res){ return res.customer_id; }), function(val, key){
+				var tempHash = {
+					"id": val[0].customer_id,
+					"name": val[0].customer_name,
+					"orders": val
+				}
+				orders.push(tempHash);
+			})
+			console.log(orders);
+			res(err, orders);
 		});
 	},
 
@@ -24,7 +34,7 @@ var order = {
 			offset = data.page ? (data.page - 1) * limit : 0;
 		var whereQry = data.filter && data.filter_key ? "WHERE " + where  : ""
 		var selectDataQry = "SELECT cu.customer_id, concat(cu.first_name, ' ', cu.last_name) AS customer_name ,"
-			selectDataQry += "(sh.qry * it.item_price) as order_price "
+			selectDataQry += "(sh.qry * it.item_price) as item_price, it.name as item_name "
 			selectDataQry += "from customer cu "
 			selectDataQry += "JOIN shipment sh ON (sh.customer_id = cu.customer_id) "
 			selectDataQry += "JOIN item it ON (it.item_id = sh.item_id) "
